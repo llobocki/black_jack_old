ad7413bf5e5f42f1cbb45d80bfd404fe861b9aeb
Łukasz Łobocki <lukasz@lukasz-desktop.(none)>
2013-03-05 01:16:51 +0100

    pierwsza wersja

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// #include <cstdlib>
+// #include <ctime>
+#include <vector>
+// #include <algorithm>
+
+#include "card.h"
+#include "player.h"
+#include "dealer.h"
+#include "game.h"
+
+
+
+int main(void) {
+//   Card karta(Card::diamonds,1);
+//   Card deck[52];
+
+
+    std::vector<Card> deck;
+    start(deck);
+    shuffle(deck);
+
+
+    for (int i = 0; i < 52; ++i)
+        deck[i].print();
+//     std::cout << deck[i].get_colour() << ' ' << deck[i].get_value() << '\t';
+    std::cout << '\n';
+
+
+}
